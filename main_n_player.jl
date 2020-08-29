879c130e5aff64fc7be7a3ce2e5cf0ca4ebf4b91
Henrik Wolf <Henrik.Wolf@mailbox.tu-dresden.de>
2020-08-29 20:10:29 +0200

    file to debug the base simulation code

--- main_n_player.jl
+++ main_n_player.jl
@@ -7,9 +7,8 @@ include("./ring_np_num.jl")
 p_0 = (zeros(ϕ_res) .+ 0.1)
 p_0 .-= cos.(1 * ϕ) * 0.03
 
-config = Config(10, 0.1, 1.5, 0.1, 0.3, 5, 1, π, 2, 5, 1000, 50)
+#config = Config(10, 0.1, 1.5, 0.1, 0.3, 5, 1, π, 2, 0.001, 1000, 30)
+config_small = Config_small(1, 200, 0, π, 2, 0.01, 1000, 30)
+p_end = solve_time_evolution(p_0, ϕ, config_small)
 
-#config_small = Config_small(10, 1, 3, π, 4, 5, 1000, 10)
-p_end = solve_time_evolution(p_0, ϕ, config)
-
-plot_result([p_end; config])
+plot_result([p_end; config_small])
