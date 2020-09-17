4cd577cbecd304c432155c04864f02e254144e5c
Henrik Wolf <Henrik.Wolf@mailbox.tu-dresden.de>
2020-09-17 15:06:42 +0200

    test simulation for parallel processing timing.

---
+++ test_multi.jl
@@ -0,0 +1,21 @@
+using Distributed
+@everywhere include("./ring_np_num.jl")
+
+
+@everywhere begin
+ϕ_res = 200
+ϕ = LinRange(0,2π, ϕ_res+1)[1:end-1]
+
+p_0 = (zeros(ϕ_res) .+ 0.8)
+p_0 .-= cos.(1 * ϕ) * 0.03
+end
+
+
+configs = []
+for n in [16]
+    for b in 4:0.2:8
+        push!(configs, Config_small(1, b, 0, π, n, 1, 1000, 100))
+    end
+end
+
+@time run_multi_sims(configs, ϕ_res, p_0, "test/")
