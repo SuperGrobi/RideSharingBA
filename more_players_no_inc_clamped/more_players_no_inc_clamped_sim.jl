74267243681d30119f219daa651e3dca2585cfd3
Henrik Wolf <Henrik.Wolf@mailbox.tu-dresden.de>
2020-10-15 21:08:43 +0200

    new sim file

---
+++ more_players_no_inc_clamped/more_players_no_inc_clamped_sim.jl
@@ -0,0 +1,21 @@
+using Distributed
+@everywhere include("../ring_np_num.jl")
+
+
+@everywhere begin
+    ϕ_res = 200
+    ϕ = LinRange(0,2π, ϕ_res+1)[1:end-1]
+
+    p_0 = (zeros(ϕ_res) .+ 0.15)
+    p_0 .-= cos.(1 * ϕ) * 0.03
+end
+
+
+configs = []
+for n in [16]
+    for b in 2.5:0.3:8
+        push!(configs, Config_small(1, b, 0, π, n, 0.7, 1000, 100))
+    end
+end
+
+run_multi_sims(configs, ϕ_res, p_0, 0.9, 0.1, "")
