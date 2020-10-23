d0fbb02ff5a3cb949569908efe3f492fc8dad1aa
Henrik Wolf <Henrik.Wolf@mailbox.tu-dresden.de>
2020-10-23 20:54:24 +0200

    other file to test convergence

--- more_players_no_inc_clamped/more_players_no_inc_clamped_sim.jl
+++ more_players_no_inc_clamped/more_players_no_inc_clamped_sim.jl
@@ -7,15 +7,18 @@ using Distributed
     ϕ = LinRange(0,2π, ϕ_res+1)[1:end-1]
 
     p_0 = (zeros(ϕ_res) .+ 0.15)
-    p_0 .-= cos.(1 * ϕ) * 0.03
+    p_0[40:160] .= 0.85
 end
 
 
 configs = []
 for n in [16]
     for b in 3.4:0.2:7
-        push!(configs, Config_small(1, b, 0, π, n, 1.2, 1000, 200))
+        push!(configs, Config_small(1, b, 0, π, n, 6/b, 1000, 200))
     end
 end
-wiggle_every = 10
-run_multi_sims(configs, ϕ_res, p_0, wiggle_every, "")
+
+
+smooth_every = 10
+kernel_length = 41
+run_multi_sims(configs, ϕ_res, p_0, smooth_every, kernel_length, "")
