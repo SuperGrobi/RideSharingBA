8b88b56e423c6606adb58b35c3475390536d7d3f
Henrik Wolf <Henrik.Wolf@mailbox.tu-dresden.de>
2020-10-24 12:24:43 +0200

    start all sims at once and hope for the best.

---
+++ more_players_no_inc_high_res/start_all_sims.jl
@@ -0,0 +1,4 @@
+include("./8_players_no_inc_high_res_sim.jl")
+include("./16_players_no_inc_high_res_sim.jl")
+include("./8_players_no_inc_high_res_high_start_sim.jl")
+include("./16_players_no_inc_high_res_high_start_sim.jl")
