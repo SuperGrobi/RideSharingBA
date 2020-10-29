6008c9f6afbbdba24d442126d57341b987b6f532
Henrik Wolf <Henrik.Wolf@mailbox.tu-dresden.de>
2020-10-29 13:51:47 +0100

    trying to fix the predictions

--- average_detour.jl
+++ average_detour.jl
@@ -5,45 +5,54 @@ include("./ring_np_num.jl")
 ϕ = LinRange(0,2π, ϕ_res+1)[1:end-1]
 
 
-function get_b(n, Θ, c, games, ϕ_res)
+
+
+function get_b(n, Θ, c, games, ϕ_res, my_ind=1)
     ϕ = LinRange(0,2π, ϕ_res+1)[1:end-1]
 
+    # build peak with start at first index and end at angle Θ
     share_end = floor(Int, Θ/2π * ϕ_res)
+    #my_ind = floor(Int, share_end/2)
     p_share = zeros(ϕ_res)
     p_share[1:share_end] .= 1
 
+    # setup arrays
     detours = []
-    matched = []
+    matched = Array{Bool, 1}()
 
     for i in 1:games
-        my_ind = rand(collect(1:length(ϕ))[p_share .== 1])
+
+        # index of destinations of n-1 other players (constant random distribution)
         ϕ_ind = rand(1:length(ϕ), n-1)
 
-        #my_share = p_share[my_ind]
+        # array that tells us if players with destinations in φ_ind want to share
         realisations_share = [sample([false,true], Weights([1-p_share[i], p_share[i]])) for i in ϕ_ind]
 
-        #my_ind = 1
-        my_share = 1
+        # get index of destination of the sharing partner in φ
+        # if other player sits at at angle = 0 (element with index 1 in φ) (0 if no match)
+        index = get_share_config(my_ind, ϕ_ind, ϕ, realisations_share)
 
-        if my_share == 1
-            index = get_share_config(my_ind, ϕ_ind, ϕ, realisations_share)
-            if index != 0
-                ang = abs(ϕ[my_ind] - ϕ[index])
-                ang = ang > π ? 2π - ang : ang
-                dist = cos_satz(ang)
-                push!(matched, true)
-            else
-                push!(matched, false)
-                dist = 0
-            end
-            push!(detours, dist/2)
-        else
+        if index != 0
+            # get destination angle of matched player and clean it up
+            ang = ϕ[index]
+            ang = ang > π ? 2π - ang : ang
+
+            # calculate detour distance
+            dist = cos_satz(ang)
+            # tell the array that in this case the player at 1 got matched
+            push!(matched, true)
+        else  # no detour and no match
             dist = 0
+            push!(matched, false)
         end
+        push!(detours, dist/2)  # add halve of the detour in detours array
     end
+
+    # calculate means
     mhd = mean(detours)
     p_match = mean(matched)
-    return (1-p_match*c) / mhd
+
+    return (1-p_match*c) / mhd  # this should be b
 end
 
 function get_crit_b(n, c, games, ϕ)
