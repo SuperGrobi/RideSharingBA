ca313269e36e26ddd83da1bd921f0d621663e8ea
Henrik Wolf <henrik.wolf@mailbox.tu-dresden.de>
2020-11-06 18:05:59 +0100

    added some plotting

--- no_inc_multi_players_plot.jl
+++ no_inc_multi_players_plot.jl
@@ -5,6 +5,9 @@ include("load_and_process.jl")
 
 ϕ_res = 200
 ϕ = LinRange(0,2π, ϕ_res+1)[1:end-1]
+
+
+
 # 8 players
 b8_low, s8_low, _ = load_run("more_players_no_inc_final/8_low/")
 s8_low_widths = [simple_width(ϕ, p[:,end]) for p in s8_low]
@@ -27,13 +30,22 @@ s16_high_widths = [simple_width(ϕ, p[:,end]) for p in s16_high]
 plot!(b16_low, s16_low_widths, label="16 players, low start")
 plot!(b16_high, s16_high_widths, label="16 players, high start")
 
-#=
-# 32 players
-b32_low, s32_low, _ = load_run("more_players_no_inc_high_res/32_low/")
-s32_low = shift_end_to_beginning.(s32_low);
-s32_low_widths = [simple_width(ϕ, p[:,end]) for p in s32_low]
-s32_low_widths[1] = 2π;
 
+
+# 32 players
+b32_low, s32_low, _ = load_run("more_players_no_inc_final/32_low/")
+s32_low_widths = [simple_width(ϕ, p[:,end]) for p in s32_low]
+
+b32_high, s32_high, _ = load_run("more_players_no_inc_final/32_high/")
+s32_high_widths = [simple_width(ϕ, p[:,end]) for p in s32_high]
+
+plot!(b32_low, s32_low_widths, label="32 players, low start", marker=:x)
+plot!(b32_high, s32_high_widths, label="32 players, high start")
+
+
+
+
+#=
 # 40 players
 b40_low, s40_low, c40_low, _ = load_run("more_players_no_inc_high_res/40_low/")
 s40_low = shift_end_to_beginning.(s40_low);
