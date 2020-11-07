0397ec8cd470edea7dae550c868208b0004110e6
Henrik Wolf <henrik.wolf@mailbox.tu-dresden.de>
2020-11-07 15:19:58 +0100

    sim for 31 players

---
+++ more_players_no_inc_final/31p_0c_high_p_sim.jl
@@ -0,0 +1,24 @@
+using Distributed
+@everywhere include("../ring_np_num.jl")
+
+
+@everywhere begin
+    n = 31
+    ϕ_res = 200
+    ϕ = LinRange(0,2π, ϕ_res+1)[1:end-1]
+
+    p_0 = (zeros(ϕ_res) .+ 0.9)
+    p_0 .-= cos.(1 * ϕ) * 0.03
+end
+
+configs = []
+for b in 11:0.2:12.8
+    push!(configs, Config_small(1, b, 0, π, n, 6/b, 1000, 300))
+end
+
+smooth_every = 10
+kernel_length = 41
+
+
+println("################### starting multi sim ###################")
+run_multi_sims(configs, ϕ_res, p_0, smooth_every, kernel_length, "$(n)_high/")
