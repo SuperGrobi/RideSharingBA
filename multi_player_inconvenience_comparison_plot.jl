b5401a9936454066df047cc06a046c54a083534f
Henrik Wolf <henrik.wolf@mailbox.tu-dresden.de>
2020-11-08 12:54:40 +0100

    added 8 players c=0.6 to plot

--- multi_player_inconvenience_comparison_plot.jl
+++ multi_player_inconvenience_comparison_plot.jl
@@ -18,6 +18,23 @@ begin
     b8_high, s8_high, _ = load_run("more_players_no_inc_final/8_high/")
     s8_high_widths = [simple_width(ϕ, p[:,end]) for p in s8_high]
 
+    # 8 players, inconvenience 0.2
+    b8_c02_low, s8_c02_low, _ = load_run("more_players_with_inc/8p/c=0.2/8_low/")
+    s8_c02_low_widths = [simple_width(ϕ, p[:,end]) for p in s8_c02_low]
+
+    b8_c02_high, s8_c02_high, _ = load_run("more_players_with_inc/8p/c=0.2/8_high/")
+    s8_c02_high_widths = [simple_width(ϕ, p[:,end]) for p in s8_c02_high]
+
+
+    # 8 players, inconvenience 0.6
+    b8_c06_low, s8_c06_low, _ = load_run("more_players_with_inc/8p/c=0.6/8_low/")
+    s8_c06_low_widths = [simple_width(ϕ, p[:,end]) for p in s8_c06_low]
+
+    b8_c06_high, s8_c06_high, _ = load_run("more_players_with_inc/8p/c=0.6/8_high/")
+    s8_c06_high_widths = [simple_width(ϕ, p[:,end]) for p in s8_c06_high]
+
+
+
 
     # 8 players, inconvenience 1
     b8_c1_low, s8_c1_low, _ = load_run("more_players_with_inc/8p/c=1/8_low/")
@@ -29,6 +46,13 @@ begin
 
     p1 = plot(b8_low, s8_low_widths, label="low start, c=0")
     plot!(p1, b8_high, s8_high_widths, label="high start, c=0")
+
+    plot!(p1, b8_c02_low, s8_c02_low_widths, label="low start, c=0.2")
+    plot!(p1, b8_c02_high, s8_c02_high_widths, label="high start, c=0.2")
+
+
+    plot!(p1, b8_c06_low, s8_c06_low_widths, label="low start, c=0.6")
+    plot!(p1, b8_c06_high, s8_c06_high_widths, label="high start, c=0.6")
 
 
     plot!(p1, b8_c1_low, s8_c1_low_widths, label="low start, c=1")
