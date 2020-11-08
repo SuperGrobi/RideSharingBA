41c9ca4fa95d6cd5b7b186b84e97f35e7424c993
Henrik Wolf <henrik.wolf@mailbox.tu-dresden.de>
2020-11-08 12:54:12 +0100

    sim files for p=16, c=0.2, 0.6, 1.0

---
+++ more_players_with_inc/16p/c=0.2/16p_0.2c_high_p_sim.jl
@@ -0,0 +1,25 @@
+using Distributed
+@everywhere include("../../../ring_np_num.jl")
+
+
+@everywhere begin
+    n = 16
+    c = 0.2
+    ϕ_res = 200
+    ϕ = LinRange(0,2π, ϕ_res+1)[1:end-1]
+
+    p_0 = (zeros(ϕ_res) .+ 0.9)
+    p_0 .-= cos.(1 * ϕ) * 0.03
+end
+
+configs = []
+for b in 4:0.4:6
+    push!(configs, Config_small(1, b, c, π, n, 6/b, 1000, 300))
+end
+
+smooth_every = 10
+kernel_length = 41
+
+
+println("################### starting multi sim ###################")
+run_multi_sims(configs, ϕ_res, p_0, smooth_every, kernel_length, "$(n)_high/")
---
+++ more_players_with_inc/16p/c=1/16p_1.0c_low_p_sim.jl
@@ -0,0 +1,40 @@
+using Distributed
+@everywhere include("../../../ring_np_num.jl")
+
+
+@everywhere begin
+    n = 16
+    ϕ_res = 200
+    ϕ = LinRange(0,2π, ϕ_res+1)[1:end-1]
+
+    p_0 = (zeros(ϕ_res) .+ 0.15)
+    p_0 .-= cos.(1 * ϕ) * 0.03
+end
+
+configs = []
+for b in 0.4:0.4:7
+    push!(configs, Config_small(1, b, 0, π, n, 6/b, 1000, 300))
+end
+
+smooth_every = 10
+kernel_length = 41
+
+reverse!(configs)
+# create long simulation object
+seed_config = configs[1]
+seed_config.steps = 100
+
+println("################### seed simulation ###################")
+seed_prob = solve_time_evolution(p, ϕ, seed_config, smooth_every, kernel_length)
+p_track_start = seed_prob[1][:,end]
+
+# reset seed config
+configs[1].steps = configs[2].steps
+
+println("################### starting multi sim ###################")
+run_multi_track(configs, ϕ_res, p_track_start, smooth_every, kernel_length, "$(n)_low/")
+
+println("################### small b_explicit ###################")
+s_b_conf = Config_small(1, 0.1, 0, π, n, 14, 1000, 300)
+small_b = solve_time_evolution(p_0, ϕ, s_b_conf, smooth_every, kernel_length)
+save_sim(small_b, s_b_conf, "$(n)_low/")
