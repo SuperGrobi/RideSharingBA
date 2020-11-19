5b18915a607273899ea2fa9d6d1b076713e99974
Henrik Wolf <henrik.wolf@mailbox.tu-dresden.de>
2020-11-19 21:57:39 +0100

    moved plots arround

--- more_players_with_inc/32p/c=1/32p_1.0c_low_p_sim.jl
+++ more_players_with_inc/32p/c=1/32p_1.0c_low_p_sim.jl
@@ -13,7 +13,7 @@ using Distributed
 end
 
 configs = []
-for b in 0.4:0.4:6
+for b in 0.4:0.4:4
     push!(configs, Config_small(1, b, c, π, n, 6/b, 1000, 300))
 end
 
---
+++ plotting_scripts/readme.txt
@@ -0,0 +1,2 @@
+these codes have to be run from the parent directory, as they read an write data relative to the file structure.
+Also, these are nearly undocumented.
